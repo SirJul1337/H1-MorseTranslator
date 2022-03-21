28fbba1b26bfcdc32ffe2bb72e988bfd3e79a7e2
niklas jul <niklas.jul@julit.dk>
2022-03-21 23:02:43 +0100

    Add project files.

---
+++ MorseTranslator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace MorseTranslator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Put in the sentence you want to translate");
+            string input = Console.ReadLine();
+            char[] chars = input.ToCharArray();
+            StringBuilder morse = new StringBuilder();
+            foreach (int i in chars) {
+                switch (chars[i])
+                {
+                    case 'a':
+                        morse.Append(".-");
+                        i++;
+                        break;
+                    case 'b':
+                        morse.Append("-...");
+                        i++;
+                        break;
+                } 
+            }
+            Console.WriteLine(morse);
+        }
+    }
+}
