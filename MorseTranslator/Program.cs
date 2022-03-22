bb55f1f932472af3ca181d97ca4286b158d79e55
nj <niklas@julit.dk>
2022-03-22 09:20:12 +0100

    Now translates the the text to morse

--- MorseTranslator/Program.cs
+++ MorseTranslator/Program.cs
@@ -7,25 +7,107 @@ namespace MorseTranslator
     {
         static void Main(string[] args)
         {
-           
+          
             Console.WriteLine("Put in the sentence you want to translate");
             string input = Console.ReadLine();
-            char[] chars = input.ToCharArray();
+            char[] chars = input.ToLower().ToCharArray();
             StringBuilder morse = new StringBuilder();
-            foreach (int i in chars) {
+            for (int i = 0; i < input.Length; i++) {
                 switch (chars[i])
                 {
                     case 'a':
                         morse.Append(".-");
-                        i++;
                         break;
                     case 'b':
                         morse.Append("-...");
-                        i++;
                         break;
+                    case'c':
+                        morse.Append("-.-.");
+                        break;
+                    case'd':
+                        morse.Append("-..");
+                        break;
+                    case'e':
+                        morse.Append(".");
+                        break;
+                    case 'f':
+                        morse.Append("..-");
+                        break;
+                    case 'g':
+                        morse.Append("--.");
+                        break;
+                    case 'h':
+                        morse.Append("....");
+                        break;
+                    case 'i':
+                        morse.Append("..");
+                        break;
+                    case 'j':
+                        morse.Append(".---");
+                        break;
+                    case 'k':
+                        morse.Append("-.-");
+                        break;
+                    case 'l':
+                        morse.Append(".-..");
+                        break;
+                    case 'm':
+                        morse.Append("--");
+                        break;
+                    case 'n':
+                        morse.Append("-.");
+                        break;
+                    case 'o':
+                        morse.Append("---");
+                        break;
+                    case 'p':
+                        morse.Append(".--.");
+                        break;
+                    case 'q':
+                        morse.Append("--.-");
+                        break;
+                    case 'r':
+                        morse.Append(".-.");
+                        break;
+                    case 's':
+                        morse.Append("...");
+                        break;
+                    case 't':
+                        morse.Append("-");
+                        break;
+                    case 'u':
+                        morse.Append("..-");
+                        break;
+                    case 'v':
+                        morse.Append("...-");
+                        break;
+                    case 'w':
+                        morse.Append(".--");
+                        break;
+                    case 'x':
+                        morse.Append("-..-");
+                        break;
+                    case 'y':
+                        morse.Append("-.--");
+                        break;
+                    case 'z':
+                        morse.Append("--..");
+                        break;
+                    case 'æ':
+                        morse.Append(".-.-");
+                        break;
+                    case 'ø':
+                        morse.Append("---.");
+                        break;
+                    case 'å':
+                        morse.Append(".--.-");
+                        break;
+                            
+
+
                 } 
             }
-            Console.WriteLine(morse);
+            Console.WriteLine(morse.ToString());
         }
     }
 }
